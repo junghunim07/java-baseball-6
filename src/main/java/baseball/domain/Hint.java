77969b32cbec83681e5ad1fba3fc61f301c99cc3
imjunghun <dlawndgns9@naver.com>
2024-08-28 02:15:45 +0900

    fix: 출력 문구 변수명 수정 및 게임 종료 여부 flag 추가

--- src/main/java/baseball/domain/Hint.java
+++ src/main/java/baseball/domain/Hint.java
@@ -4,13 +4,17 @@ public class Hint {
 
     private int strike;
     private int ball;
+    private boolean endFlag;
 
     public void compare(Numbers computer, Numbers player) {
         this.strike = computer.countSame(player);
         this.ball = computer.countEqual(player);
+        this.endFlag = false;
     }
 
     public String getHint() {
+        if (strike == 3)
+            endFlag = true;
         if (strike != 0 && ball != 0)
             return ball + "볼 " + strike + "스트라이크";
         if (strike == 0 && ball != 0)
--- src/main/java/baseball/ui/OutputPrinter.java
+++ src/main/java/baseball/ui/OutputPrinter.java
@@ -2,25 +2,25 @@ package baseball.ui;
 
 public class OutputPrinter {
 
-    private static final String start = "숫자 야구 게임을 시작합니다.";
-    private static final String progress = "숫자를 입력해주세요 : ";
-    private static final String end = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    private static final String restart = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String START = "숫자 야구 게임을 시작합니다.";
+    private static final String PROGRESS = "숫자를 입력해주세요 : ";
+    private static final String END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static void printStart() {
-        System.out.println(start);
+        System.out.println(START);
     }
 
     public static void printProgress() {
-        System.out.print(progress);
+        System.out.print(PROGRESS);
     }
 
     public static void printEnd() {
-        System.out.println(end);
+        System.out.println(END);
     }
 
     public static void printRestart() {
-        System.out.println(restart);
+        System.out.println(RESTART);
     }
 
     public static void print(String hint) {
