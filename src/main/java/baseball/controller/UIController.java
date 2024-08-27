a4d789359e55cf85750923f6e9d2bf1fcb28bacb
imjunghun <dlawndgns9@naver.com>
2024-08-28 01:36:32 +0900

    feat: 기본 문구 출력 기능

--- src/main/java/baseball/controller/UIController.java
+++ src/main/java/baseball/controller/UIController.java
@@ -1,10 +1,27 @@
 package baseball.controller;
 
 import baseball.ui.InputReader;
+import baseball.ui.OutputPrinter;
 
 public class UIController {
 
-    public String receive() {
+    public static String receive() {
         return InputReader.readUserValue();
+    }
+
+    public static void printOpenPhrase() {
+        OutputPrinter.printStart();
+    }
+
+    public static void printProgressPhrase() {
+        OutputPrinter.printProgress();
+    }
+
+    public static void printEndPhrase() {
+        OutputPrinter.printEnd();
+    }
+
+    public static void printRestartPhrase() {
+        OutputPrinter.printRestart();
     }
 }
